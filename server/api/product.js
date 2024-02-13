aab5a63828f4b93efd431af7a7ce925efb568788
Farras Arkan <farras.arkan@student.uns.ac.id>
2024-02-14 04:19:45 +0700

    add(media-middleware) : add uploadmedia middleware

--- server/api/product.js
+++ server/api/product.js
@@ -3,6 +3,11 @@ const Router = require("express").Router();
 const Validation = require("../helpers/validationHelper");
 const ProductHelper = require("../helpers/productHelper");
 const GeneralHelper = require("../helpers/generalHelper");
+const {
+  uploadToCloudinary,
+  cloudinaryDeleteImg,
+} = require("../../utils/cloudinary");
+const uploadMedia = require("../middlewares/uploadMedia");
 
 const getAllProduct = async (req, res) => {
   try {
@@ -26,14 +31,35 @@ const getProductById = async (req, res) => {
 };
 
 const createProduct = async (req, res) => {
+  let imageResult;
   try {
+    if (req?.fileValidationError)
+      return responseError(
+        res,
+        400,
+        "Bad Request",
+        req.fileValidationError.message
+      );
+    if (!req?.files?.image_url)
+      return res.status(400).json({ message: "Image is required" });
+
     Validation.productValidation(req.body);
 
-    await ProductHelper.createProduct(req.body);
+    imageResult = await uploadToCloudinary(req.files.image_url[0], "image");
+
+    if (!imageResult?.url)
+      return res.status(500).json({ message: imageResult.error });
+
+    await ProductHelper.createProduct({
+      ...req.body,
+      image_url: imageResult?.url,
+    });
 
     return res.status(200).json("Product successfully created");
   } catch (err) {
-    console.log(err);
+    if (imageResult?.public_id) {
+      await cloudinaryDeleteImg(imageResult.public_id, "image");
+    }
     return res.send(GeneralHelper.errorResponse(err));
   }
 };
@@ -63,7 +89,11 @@ const deleteProduct = async (req, res) => {
 
 Router.get("/", getAllProduct);
 Router.get("/:id", getProductById);
-Router.post("/create", createProduct);
+Router.post(
+  "/create",
+  uploadMedia.fields([{ name: "image_url", maxCount: 1 }]),
+  createProduct
+);
 Router.put("/update/:id", updateProduct);
 Router.delete("/delete/:id", deleteProduct);
 
